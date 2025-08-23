27e2fefac4a3893a699ce19083be54c5c896bc5a
Nareshindu <nareshtammineni0@gmail.com>
2025-08-23 15:54:12 +0530

    pushing expense project dockerfiles to github

--- dockerfiles/backend/src/main/java/com/expense/demo/controller/EntryController.java
+++ dockerfiles/backend/src/main/java/com/expense/demo/controller/EntryController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/entries")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://expense-frontend:3000")
 public class EntryController {
     @Autowired
     private EntryRepository entryRepository;
